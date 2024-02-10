f743b3bf06e46e16f220754fee47a71722849cb9
taufan12345 <abaysurbakti17@gmail.com>
2024-02-10 13:52:12 +0700

    WN-6 View Add & Edit News

--- app/news/add/page.tsx
+++ app/news/add/page.tsx
@@ -5,60 +5,58 @@ import Link from "next/link";
 import Navbar from "@/components/Navbar";
 
 // icons
-import { IconBatteryLoading } from "@irsyadadl/paranoid";
+import { IconArrowLeft } from "@irsyadadl/paranoid";
 
 export default function NewsAdd() {
   return (
     <>
       <Navbar active={2} />
-
       <main className="px-20 mt-10">
-        <section className="bg-white rounded shadow p-10">
-          <div className="flex justify-between items-center mb-10">
+        <section className="shadow-xl p-7 bg-white rounded mb-10">
+          <div className="flex justify-between items-center">
+            <h2 className="font-bold">ADD NEWS</h2>
+            <Link
+              href={"/news"}
+              className="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-none flex gap-2"
+            >
+              <IconArrowLeft />
+              back
+            </Link>
+          </div>
+          <div className="grid grid-cols-3 gap-4">
             <div className="">
-              <h1 className="text-xl font-bold">ADD NEWS</h1>
+              <h2 className="mb-2">Banner</h2>
+              <img src="/img-placeholder.png" alt="" className="w" />
             </div>
-            <div>
-              <Link
-                href={"/news"}
-                className="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-none flex gap-2"
-              >
-                <IconBatteryLoading />
-                back
-              </Link>
+            <div className="col-span-2">
+              <div className="mb-5">
+                <label htmlFor="title">Title</label>
+                <input
+                  className="w-full py-3 px-5 rounded border"
+                  type="text"
+                  id="title"
+                />
+              </div>
+              <div className="mb-5">
+                <label htmlFor="content">Content</label>
+                <textarea
+                  className="w-full max-h-44 min-h-44 py-3 px-5 rounded border"
+                  name=""
+                  id="content"
+                ></textarea>
+              </div>
+              <div className="mb-5">
+                <label htmlFor="writer">Writer</label>
+                <input
+                  className="w-full py-3 px-5 rounded border"
+                  type="text"
+                  id="writer"
+                />
+              </div>
+              <button className="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                submit
+              </button>
             </div>
-          </div>
-          <div className="mb-5">
-            <label htmlFor="title">Title</label>
-            <input
-              className="w-full py-3 px-5 rounded border"
-              type="text"
-              id="title"
-            />
-          </div>
-          <div className="mb-5">
-            <label htmlFor="content">Content</label>
-            <textarea
-              className="w-full max-h-44 min-h-44 py-3 px-5 rounded border"
-              name=""
-              id="content"
-            ></textarea>
-          </div>
-          <div className="mb-5">
-            <label htmlFor="writer">Writer</label>
-            <input
-              className="w-full py-3 px-5 rounded border"
-              type="text"
-              id="writer"
-            />
-          </div>
-          <div className="flex justify-end gap-2 mx-10 mt-5">
-            <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded">
-              Submit
-            </button>
-            <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded">
-              Submit
-            </button>
           </div>
         </section>
       </main>
--- app/news/edit/page.tsx
+++ app/news/edit/page.tsx
@@ -1,54 +1,65 @@
 import React from "react";
+import Link from "next/link";
 
-export default function NewsEdit() {
+// components
+import Navbar from "@/components/Navbar";
+
+// icons
+import { IconArrowLeft } from "@irsyadadl/paranoid";
+
+export default function NewsAdd() {
   return (
-    <div>
-      {" "}
-      <div className="">
-        <div className="flex mx-10 mt-5 items-center">
-          <div className="">
-            <h1>ADD EVENT</h1>
-          </div>
-          <div className="ml-[64rem]">
-            <button className="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-none">
+    <>
+      <Navbar active={2} />
+      <main className="px-20 mt-10">
+        <section className="shadow-xl p-7 bg-white rounded mb-10">
+          <div className="flex justify-between items-center">
+            <h2 className="font-bold">EDIT</h2>
+            <Link
+              href={"/news"}
+              className="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-none flex gap-2"
+            >
+              <IconArrowLeft />
               back
-            </button>
+            </Link>
           </div>
-        </div>
-        <div className="mx-10 mt-5 items-center">
-          <div className="font-bold mb-2">
-            <h1>Title</h1>
+          <div className="grid grid-cols-3 gap-4">
+            <div className="">
+              <h2 className="mb-2">Banner</h2>
+              <img src="/img-placeholder.png" alt="" className="w" />
+            </div>
+            <div className="col-span-2">
+              <div className="mb-5">
+                <label htmlFor="title">Title</label>
+                <input
+                  className="w-full py-3 px-5 rounded border"
+                  type="text"
+                  id="title"
+                />
+              </div>
+              <div className="mb-5">
+                <label htmlFor="content">Content</label>
+                <textarea
+                  className="w-full max-h-44 min-h-44 py-3 px-5 rounded border"
+                  name=""
+                  id="content"
+                ></textarea>
+              </div>
+              <div className="mb-5">
+                <label htmlFor="writer">Writer</label>
+                <input
+                  className="w-full py-3 px-5 rounded border"
+                  type="text"
+                  id="writer"
+                />
+              </div>
+              <button className="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                submit
+              </button>
+            </div>
           </div>
-          <div className="border-2">
-            <input className="w-full" type="text" />
-          </div>
-        </div>
-        <div className="mx-10 mt-5 items-center">
-          <div className="font-bold mb-2">
-            <h1>Content</h1>
-          </div>
-          <div className="border-2">
-            <textarea
-              className="w-full max-h-44 min-h-44"
-              name=""
-              id=""
-            ></textarea>
-          </div>
-        </div>
-        <div className="mx-10 mt-5 items-center">
-          <div className="font-bold mb-2">
-            <h1>Writer</h1>
-          </div>
-          <div className="border-2">
-            <input className="w-full" type="text" />
-          </div>
-        </div>
-        <div className="mx-10 mt-5 items-center">
-          <button className="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-none">
-            Submit
-          </button>
-        </div>
-      </div>
-    </div>
+        </section>
+      </main>
+    </>
   );
 }
